68eb6680763eb9e3e74b2888d8a2845afd9ca5b0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 14:15:29 +1200

    Started on some python code

--- database_assesment.py
+++ database_assesment.py
@@ -1,1 +1,20 @@
-print('hello')
+'''
+docstring
+'''
+
+import sqlite3
+
+Database = "clashroyale_database.db"
+
+with sqlite3.connect(Database) as db:
+    cursor = db.cursor()
+    sql = "SELECT * FROM card;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #prints nicely
+    for card in results:
+        print(f"ID: {card[0]} Name: {card[8]} Elixir: {card[4]}")
+
+#closes the db
+db.close()
+
